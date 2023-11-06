a18ae5d912a8c472d888403cc8ce33751b8c282e
omargonz01 <139013469+omargonz01@users.noreply.github.com>
2023-11-06 20:36:02 +0000

    update nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,34 +1,20 @@
-import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 
-class Nav extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    );
-  }
-}
+const Navigation = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#">Navbar</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#">Home</Nav.Link>
+          <Nav.Link href="#">Lists</Nav.Link>
+          <Nav.Link href="#">Profile</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-export default Nav;
+export default Navigation;
+
