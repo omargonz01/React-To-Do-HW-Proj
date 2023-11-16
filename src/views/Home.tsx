d69dbd68459418a2cdfd1f5a6d0464476f649c7b
Omar Gonzalez <gomarzalez@gmail.com>
2023-11-15 18:16:53 -0800

    added card to home page + adtl effect

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container, Typography, Box, Button } from '@mui/material';
+import { Container, Typography, Box, Button, Card, CardContent, CardMedia } from '@mui/material';
 import '../components/Form/Home.css';
+import { Link } from 'react-router-dom';
 
 
 
@@ -13,12 +14,36 @@ const HomePage: React.FC = () => {
       <Typography variant="h5">
         Organize your tasks, increase productivity, and achieve more.
       </Typography>
-      <Box>
-        <Button className="get-started-button" variant="contained" color="primary" size="large">
-          Get Started
-        </Button>
+
+      <Box mt={4}>
         
+        <Link to="/create-todo" className="text-decoration-none">
+          <Button className="get-started-button" variant="contained" color="primary" size="large">
+            Get Started
+          </Button>
+        </Link>
       </Box>
+
+      <Box className="card-container">
+        <Card style={{ maxWidth: 400 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://source.unsplash.com/random"
+            alt="Random"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Enjoy \(￣︶￣*\)
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Thank you for using the app. Please enjoy some random photos.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+
+      {/* will place additional MUI components here */}
     </Container>
   );
 };
