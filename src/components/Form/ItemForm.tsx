0b91a8417f6b0e0d4c20bbad3297c8e3bf2ec7b5
Omar Gonzalez <gomarzalez@gmail.com>
2023-11-24 20:12:31 -0800

    Added date & time pickers sdk in ItemForm

--- src/components/Form/ItemForm.tsx
+++ src/components/Form/ItemForm.tsx
@@ -1,8 +1,14 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState } from "react";
+import Datetime from "react-datetime";
+import TimePicker from "react-time-picker";
+import "react-datetime/css/react-datetime.css";
+import "react-time-picker/dist/TimePicker.css";
+import moment from 'moment';
+
 
 interface ICreateList {
   listName: string;
-  items: { name: string; checked: boolean; dueDate: Date }[];
+  items: { name: string; checked: boolean; dueDate?: Date | string }[];
   selected: boolean;
 }
 
@@ -12,21 +18,34 @@ interface Props {
 }
 
 const ItemForm = ({ addItem }: Props) => {
-  const [itemName, setItemName] = useState('');
-  const [dueDate, setDueDate] = useState(new Date());
+  const [itemName, setItemName] = useState("");
+  const [dueDate, setDueDate] = useState<Date | string>(new Date());
+  const [dueTime, setDueTime] = useState<string | null>(null);
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    addItem(itemName, dueDate);
-    setItemName('');
+
+    // Combine date and time into a single string
+    const combinedDateTime = `${
+      dueDate instanceof Date ? dueDate.toISOString().substr(0, 10) : dueDate
+    }${dueTime ? `T${dueTime}:00.000Z` : ""}`;
+
+    // Convert the combined string to a Date object
+    const formattedDueDate = new Date(combinedDateTime);
+
+    addItem(itemName, formattedDueDate);
+    setItemName("");
     setDueDate(new Date());
+    setDueTime(null);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="itemName" className="form-label">
-          <p><u>Add Item</u></p>
+          <p>
+            <u>Add Item</u>
+          </p>
           <em>Item Name:</em>
         </label>
         <input
@@ -37,18 +56,30 @@ const ItemForm = ({ addItem }: Props) => {
           onChange={(event) => setItemName(event.target.value)}
         />
       </div>
+
       <div className="mb-3">
         <label htmlFor="dueDate" className="form-label">
           Due Date:
         </label>
-        <input
-          type="date"
-          className="form-control"
-          id="dueDate"
-          value={dueDate.toISOString().substr(0, 10)}
-          onChange={(event) => setDueDate(new Date(event.target.value))}
-        />
+        <Datetime
+  value={(dueDate instanceof Date || dueDate === null) ? dueDate : ''}
+  onChange={(date) => setDueDate(date instanceof moment ? date.toDate() : new Date())}
+  inputProps={{ className: 'form-control' }}
+/>
       </div>
+
+      <div className="mb-3">
+        <label htmlFor="dueTime" className="form-label">
+          Due Time:
+        </label>
+        <TimePicker
+  value={(dueTime === null) ? undefined : dueTime}
+  onChange={(time) => setDueTime(time || null)}
+  clearIcon={null}
+  className="form-control"
+/>
+      </div>
+
       <button type="submit" className="myButtonClass">
         Add To List
       </button>
